52bb6c0c1aee9ad06d1a7854eac1e7deb330f2fd
allanthomas_ <allanthomas_@live.com>
2015-05-12 22:01:14 -0300

    Ok

--- js/IMCService.js
+++ js/IMCService.js
@@ -1,10 +1,12 @@
-var IMCService={
-    calculate: function(height, weight){
-        
-    },
-    
-    //TODO to implemented
-    decription: Function(index){
-        return "To be implemented";
-    }
-}
+var IMCService = {
+	
+	calculate: function(height, weight) {
+		return weight / (height * height);
+	},
+	
+	//TODO to implemented
+	description: function(index) {
+		return "To be implemented";
+	}
+	
+};
